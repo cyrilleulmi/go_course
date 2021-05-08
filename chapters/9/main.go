c934cd2ee2f2cb5a7cab8ba391aab666c54ad9e7
Cyrille Ulmi <cyrille.ulmi@bluewin.ch>
2021-05-08 17:00:22 +0200

    changed structure

--- chapters/9/main.go
+++ chapters/9/main.go
@@ -6,14 +6,15 @@ func main() {
 	var myString string
 	myString = "Green"
 
-	log.Println("myString is set to ", myString)
+	log.Println("myString is set to:", myString)
 
 	changeUsingPointer(&myString)
-	log.Println("after func call: ", myString)
+	log.Println("after func call:", myString)
 
 }
 
 func changeUsingPointer(s *string) {
+	log.Println("s is set to:", s)
 	newValue := "Red"
 	*s = newValue
 }
