20fda1a9a431977282b50881db399f78db996ae9
Cyrille Ulmi <cyrille.ulmi@bluewin.ch>
2022-01-19 20:39:59 +0100

    finished section 15

--- section_15/section15.go
+++ section_15/section15.go
@@ -1,6 +1,9 @@
 package section15
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func ex1() {
 	i := ex_1_foo()
@@ -66,4 +69,93 @@ type person struct {
 
 func (p person) speak() {
 	fmt.Println("hi, I am", p.first, p.last)
+}
+
+func ex5() {
+	mySquare := square{side: 10}
+	myCircle := circle{radius: 10}
+
+	info(mySquare)
+	info(myCircle)
+}
+
+type square struct {
+	side float32
+}
+
+type circle struct {
+	radius float32
+}
+
+type shape interface {
+	area() float32
+}
+
+func (s square) area() float32 {
+	return s.side * s.side
+}
+
+func (c circle) area() float32 {
+	return c.radius * c.radius * math.Pi
+}
+
+func info(s shape) {
+	println("the shape has an area of", s.area())
+}
+
+func ex6() {
+	println(func() string {
+		return "hello there"
+	}())
+}
+
+func ex7() {
+	myFunc := func() string {
+		return "general kenobi!"
+	}
+
+	println(myFunc())
+}
+
+func ex8() {
+	myFuncOfAFunc := funcOfAFunc()
+	println(myFuncOfAFunc())
+}
+
+func funcOfAFunc() func() string {
+	return func() string { return "func inside func" }
+}
+
+func ex9() {
+	evenOrOdd(1, evenCallback, oddCallback)
+	evenOrOdd(2, evenCallback, oddCallback)
+	evenOrOdd(3, evenCallback, oddCallback)
+	evenOrOdd(4, evenCallback, oddCallback)
+}
+
+func evenCallback() {
+	println("even!")
+}
+
+func oddCallback() {
+	println("odd!")
+}
+
+func evenOrOdd(number int, evenCallback func(), oddCallback func()) {
+	if number%2 == 0 {
+		evenCallback()
+	} else {
+		oddCallback()
+	}
+}
+
+func ex10() {
+	{
+		i := 20
+		println(i)
+	}
+	{
+		i := 40
+		println(i)
+	}
 }
--- section_15/section15_test.go
+++ section_15/section15_test.go
@@ -17,3 +17,27 @@ func TestEx3(t *testing.T) {
 func TestEx4(t *testing.T) {
 	ex4()
 }
+
+func TestEx5(t *testing.T) {
+	ex5()
+}
+
+func TestEx6(t *testing.T) {
+	ex6()
+}
+
+func TestEx7(t *testing.T) {
+	ex7()
+}
+
+func TestEx8(t *testing.T) {
+	ex8()
+}
+
+func TestEx9(t *testing.T) {
+	ex9()
+}
+
+func TestEx10(t *testing.T) {
+	ex10()
+}
